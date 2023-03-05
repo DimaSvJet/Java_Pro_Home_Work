5c10f9cc39aa25d7f1f7df3976d475cc4e4fb444
DimaSv <dimasvjet@gmail.com>
2023-03-05 18:16:02 +0200

    Home_Work_6

---
+++ src/main/java/com/example/home_work_6/Car.java
@@ -0,0 +1,24 @@
+package com.example.home_work_6;
+
+public class Car {
+
+    public void start(){
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+
+    }
+
+    private void startElectricity() {
+
+    }
+
+    private void startCommand() {
+
+    }
+
+    private void startFuelSystem(){
+
+    }
+
+}
